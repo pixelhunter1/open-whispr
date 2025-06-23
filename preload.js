5a5db0942ee24da5f435e102efd982aaecd9d964
Gabriel Stein <gabrielstein416@gmail.com>
2025-06-23 11:00:36 -0700

    final clean-up

--- preload.js
+++ preload.js
@@ -4,7 +4,4 @@ contextBridge.exposeInMainWorld('electronAPI', {
   pasteText: (text) => ipcRenderer.invoke('paste-text', text),
   hideWindow: () => ipcRenderer.invoke('hide-window'),
   onToggleDictation: (callback) => ipcRenderer.on('toggle-dictation', callback),
-});
-
-// Expose the API key securely
-contextBridge.exposeInMainWorld('OPENAI_API_KEY', process.env.OPENAI_API_KEY || ''); 
+}); 
